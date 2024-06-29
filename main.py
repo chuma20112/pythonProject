757da0c10939de1707225e36723e2146c59af0c0
Chuma20112 <chuma0489@gmail.com>
2024-06-29 12:27:55 +0300

    start project. V0.1

---
+++ main.py
@@ -0,0 +1,22 @@
+from game_gonfig import *
+from chess_items import *
+
+clock = pg.time.Clock()
+screen = pg.display.set_mode(WINDOW_SIZE)
+screen.fill(BACKGROUND)
+
+chessboard = ChessBoard(screen)
+
+run = True
+while run:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            run = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            chessboard.btn_down(event.button, event.pos)
+        if event.type == pg.MOUSEBUTTONUP:
+            chessboard.btn_up(event.button, event.pos)
+        if event.type == pg.MOUSEMOTION:
+            chessboard.drag(event.pos)
